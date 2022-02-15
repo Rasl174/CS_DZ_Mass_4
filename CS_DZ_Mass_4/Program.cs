21ddcbe7ec4853c907d614bf75551d6574484e2e
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-15 12:22:26 +0500

    Изменил

--- CS_DZ_Mass_4/Program.cs
+++ CS_DZ_Mass_4/Program.cs
@@ -11,27 +11,38 @@ namespace CS_DZ_Mass_4
         static void Main(string[] args)
         {
             int[] array = new int[0];
-            bool exit = false;
+            bool canExit = false;
 
             Console.WriteLine("Для сложения введите sum. Для выхода введите exit.");
 
-            while (!exit)
+            while (!canExit)
             {
                 Console.Write("Введите числa которые нужно сложить , sum или exit: ");
                 string userInput = Console.ReadLine();
 
-                if (userInput.ToLower() == "sum")
+
+
+                switch (userInput)
                 {
-                    int arraySum = 0;
-                    for (int i = 0; i < array.Length; i++)
-                    {
-                        arraySum += array[i];
-                    }
-                    Console.Clear();
-                    Console.WriteLine("Сумма: " + arraySum);
+                    case "sum":
+
+                        int arraySum = 0;
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            arraySum += array[i];
+                        }
+                        Console.Clear();
+                        Console.WriteLine("Сумма: " + arraySum);
+                        break;
+
+                    case "exit":
+
+                        canExit = true;
+                        break;
+
                 }
 
-                else if (int.TryParse(userInput, out int number))
+                if (int.TryParse(userInput, out int numbers))
                 {
                     int[] tempArray = new int[array.Length + 1];
                     tempArray[tempArray.Length - 1] = int.Parse(userInput);
@@ -41,12 +52,7 @@ namespace CS_DZ_Mass_4
                     }
                     array = tempArray;
                 }
-
-                else if (userInput.ToLower() == "exit")
-                {
-                    exit = true;
-                }
             }
         }
-    }    
+    }
 }
